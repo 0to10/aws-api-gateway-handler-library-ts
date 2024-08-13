7b3f0d39825b33d121b76ba8b4cd037b9c7d7550
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-08-13 21:21:27 +0200

    Added HttpStatusCode enum with all client error codes.
    Signed-off-by: 0TO10 <121996729+0to10-dev@users.noreply.github.com>

---
+++ src/HTTP/HttpStatusCode.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * HttpStatusCode
+ *
+ * @copyright Copyright (c) 2024 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export enum HttpStatusCode {
+    // Client error
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+    NOT_ACCEPTABLE = 406,
+    PROXY_AUTHENTICATION_REQUIRED = 407,
+    REQUEST_TIMEOUT = 408,
+    CONFLICT = 409,
+    GONE = 410,
+    LENGTH_REQUIRED = 411,
+    PRECONDITION_FAILED = 412,
+    PAYLOAD_TOO_LARGE = 413,
+    URI_TOO_LONG = 414,
+    UNSUPPORTED_MEDIA_TYPE = 415,
+    RANGE_NOT_SATISFIABLE = 416,
+    EXPECTATION_FAILED = 417,
+    IM_A_TEAPOT = 418,
+    MISDIRECTED_REQUEST = 421,
+    UPGRADE_REQUIRED = 426,
+    PRECONDITION_REQUIRED = 428,
+    TOO_MANY_REQUESTS = 429,
+    REQUEST_HEADER_FIELDS_TOO_LARGE = 431,
+    UNAVAILABLE_FOR_LEGAL_REASONS = 451,
+
+    // Client error - Experimental
+    PAYMENT_REQUIRED = 402,
+    TOO_EARLY = 425,
+
+    // Client error - WebDAV
+    UNPROCESSABLE_CONTENT = 422,
+    LOCKED = 423,
+    FAILED_DEPENDENCY = 424,
+}
