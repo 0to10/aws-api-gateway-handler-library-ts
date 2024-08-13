1f77b771386a93dc4ed69ad31707ce5a68d3a3c5
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-08-13 12:47:36 +0200

    Added CognitoRequestInformation type.
    Signed-off-by: 0TO10 <121996729+0to10-dev@users.noreply.github.com>

---
+++ src/CognitoRequestInformation.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * CognitoRequestInformation
+ *
+ * @copyright Copyright (c) 2024 0TO10 B.V. <https://0to10.nl>
+ * @license MIT
+ */
+export type CognitoRequestInformation = {
+    identityId?: string;
+    userPoolId?: string;
+    username?: string;
+};
