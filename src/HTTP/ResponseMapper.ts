0765fb36ee64503b29a62bc1db6e13c49c4bafca
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2024-09-16 18:22:02 +0200

    Minor: Updated the ResponseMapper interface to be an abstract class.

--- src/HTTP/ResponseMapper.ts
+++ src/HTTP/ResponseMapper.ts
@@ -6,6 +6,6 @@
  * @copyright Copyright (c) 2024 0TO10 B.V. <https://0to10.nl>
  * @license MIT
  */
-export interface ResponseMapper<T> {
-    transform(input: T): object;
+export abstract class ResponseMapper<T> {
+    abstract transform(input: T): object;
 }
