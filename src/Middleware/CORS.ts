f7e5537702ff7acc603fefdd8e9a50ea79c66f99
0TO10 <121996729+0to10-dev@users.noreply.github.com>
2025-05-05 12:35:52 +0200

    Refactored the CORS middleware to return headers according to the request, limited to what the "boolean" enabling of the middleware allows. Needs to be refactored.

--- src/Middleware/CORS.ts
+++ src/Middleware/CORS.ts
@@ -11,23 +11,44 @@ import {NextFunction, Request, Response, RequestHandler} from 'express';
  */
 export class CORS {
 
+    private static credentialHeaders: [
+        'authorization',
+        'cookie',
+    ];
+
     public static handle(): RequestHandler {
         return (
             request: Request,
             response: Response,
             next: NextFunction,
         ): void => {
-            response.header('Access-Control-Allow-Headers', '*');
-            response.header('Access-Control-Allow-Methods', 'OPTIONS,HEAD,POST,GET,PUT,DELETE');
-            response.header('Access-Control-Allow-Origin', '*');
+            response.header('Access-Control-Allow-Origin', request.header('origin') ?? '*');
 
             if ('OPTIONS' === request.method) {
+                response.header('Access-Control-Allow-Headers', request.header('access-control-request-headers') ?? '*');
+                response.header('Access-Control-Allow-Methods', 'OPTIONS,HEAD,POST,GET,PUT,DELETE');
+
                 response.status(200).send('');
+
                 return;
+            }
+
+            if (CORS.requestContainsHeader(request, CORS.credentialHeaders)) {
+                response.header('Access-Control-Allow-Credentials', 'true');
             }
 
             next();
         };
+    }
+
+    private static requestContainsHeader(request: Request, headers: Array<string>): boolean {
+        for (const header of headers) {
+            if (undefined !== request.header(header)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
